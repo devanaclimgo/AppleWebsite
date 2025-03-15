16058cc08e6536a52d97b40065313e4f6172f5de
devanaclimgo <anaclimgo@gmail.com>
2025-03-14 22:56:45 -0300

    adding array div navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,14 @@ const Navbar = () => {
     <header>
       <nav>
         <img src={appleImg} alt='Apple' width={14} height={18} />
+
+        <div>
+          {['Phones', 'Macbooks', 'Tablets'].map((nav) => (
+            <div key={nav}>
+              {nav}
+            </div>
+          ))}
+        </div>
       </nav>
     </header>
   )
