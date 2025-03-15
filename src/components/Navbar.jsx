8127480c4139f68a4c1eec38c3b6e74743019108
devanaclimgo <anaclimgo@gmail.com>
2025-03-14 23:04:41 -0300

    styles navlists

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
         <div className='flex flex-1 justify-center max-sm:hidden'>
           {navLists.map((nav) => (
-            <div key={nav}>
+            <div key={nav} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>
               {nav}
             </div>
           ))}
