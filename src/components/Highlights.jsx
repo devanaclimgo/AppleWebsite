2f8b494b00f2a88cce7aacf6888b5a669789cd29
devanaclimgo <anaclimgo@gmail.com>
2025-03-16 22:18:54 -0300

    added duration and stagger to link gsap animation highlights

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 const Highlights = () => {
   useGSAP(() => {
     gsap.to('#title', { opacity: 1, y: 0 })
-    gsap.to('.link', { opacity: 1, y: 0 })
+    gsap.to('.link', { opacity: 1, y: 0, duration: 1, stagger: 0.25 })
   }, [])
 
   return (
