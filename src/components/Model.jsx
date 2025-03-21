312aee399d5450300c534ae04fae99a0595029fb
devanaclimgo <anaclimgo@gmail.com>
2025-03-21 19:54:46 -0300

    added canvas component container

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -53,6 +53,21 @@ const Model = () => {
               item={model}
               size={size}
             />
+
+            <Canvas 
+              className="w-full h-full"
+              style={{
+                position: 'fixed',
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                overflow:'hidden'
+              }}
+              eventSource={document.getElementById('root')}
+            >
+              <View.Port />
+            </Canvas>
           </div>
         </div>
 
