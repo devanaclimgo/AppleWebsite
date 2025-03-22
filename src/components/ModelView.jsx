76251c00c0236133d518db78a9f44cd82ee05ef9
devanaclimgo <anaclimgo@gmail.com>
2025-03-21 21:09:41 -0300

    added view of model view

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { View } from '@react-three/drei'
 
-const ModelView = () => {
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
   return (
-    <div>ModelView</div>
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${index === 2} ? 'right-[-100%] : ''`}
+    >
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+    </View>
   )
 }
 
