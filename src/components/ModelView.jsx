9d70122b88a2ddfe2dd64678e894efd68e832602
devanaclimgo <anaclimgo@gmail.com>
2025-03-26 21:11:00 -0300

    added orbit control component

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { PerspectiveCamera, View } from '@react-three/drei'
 
+import * as THREE from 'three'
 import Lights from './Lights'
 import IPhone from './IPhone'
 import { Suspense } from 'react'
@@ -17,6 +18,16 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, siz
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
       <Lights />
+
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
 
       <group ref={groupRef} name={`${index === 1} ? 'small' : 'large'`} position={[0, 0, 0]}>
         <Suspense fallback={<Html><div>Loading</div></Html>}>
