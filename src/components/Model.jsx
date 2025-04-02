052e4622d4c4a7531fdc904951ad7c786cc5b981
devanaclimgo <anaclimgo@gmail.com>
2025-04-02 16:03:54 -0300

    typos updated

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,9 +1,13 @@
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import ModelView from "./ModelView";
 import React, { useEffect, useRef, useState } from 'react';
 import { yellowImg } from '../utils'
 
 import * as THREE from 'three'
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+import { models, sizes } from "../constants";
 import { animateWithGsapTimeline } from '../utils/animations';
 
 const Model = () => {
