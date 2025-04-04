d6ac9d57d2d838e0be78c807c07fa08bf2659367
Ana <anaclimgo@gmail.com>
2025-04-03 22:15:49 -0300

    sentry imported and initialized

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://a4a04b0d188903ddcd272f0ec1ebd3ec@o4509092339843072.ingest.de.sentry.io/4509092342399056"
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
