2e88d772addf9e32f4bf6fca080f60f0e0d47d64
Ana <anaclimgo@gmail.com>
2025-04-05 14:38:37 -0300

    added onclick event to test sentry

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Highlights from "./components/Highlights"
-import Model from "./components/Model"
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
 
-import * as Sentry from '@sentry/react'
+import * as Sentry from '@sentry/react';
 
-const App =() => {
+const App = () => {
+  return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+
   return (
     <main className="bg-black">
       <Navbar />
@@ -13,7 +15,7 @@ const App =() => {
       <Highlights />
       <Model />
     </main>
-  )
-}
+  );
+};
 
-export default Sentry.withProfiler(App)
+export default Sentry.withProfiler(App);
