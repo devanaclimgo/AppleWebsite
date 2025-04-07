7fbd22e4bd5848aaffcf56f8cf10727c6125b55d
Ana <anaclimgo@gmail.com>
2025-04-07 15:00:10 -0300

    tracing the activity of the project on sentry set up

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,9 @@ import * as Sentry from "@sentry/react";
 
 Sentry.init({
   dsn: "https://a4a04b0d188903ddcd272f0ec1ebd3ec@o4509092339843072.ingest.de.sentry.io/4509092342399056",
-  integrations: [
-    Sentry.replayIntegration()
-  ],
-  replaysSessionSampleRate: 1.0,
-  replaysOnErrorSampleRate: 1.0 
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", new RegExp("^https://http://localhost:5174//api/")]
 });
 
 createRoot(document.getElementById('root')).render(
