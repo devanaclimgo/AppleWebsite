b1ed00ffa754e26d1fa31f27a279d27921ce8ca1
Ana <anaclimgo@gmail.com>
2025-04-07 15:06:51 -0300

    deleting testing function used for sentry

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import Model from './components/Model';
 import * as Sentry from '@sentry/react';
 
 const App = () => {
-  return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
-
   return (
     <main className="bg-black">
       <Navbar />
