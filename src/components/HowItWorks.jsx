0d28d1001023f6deaf73c55f626256964235dcf9
Ana <anaclimgo@gmail.com>
2025-04-08 12:04:41 -0300

    importing gsap

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import { chipImg, frameImg, frameVideo } from '../utils';
 import { useGSAP } from '@gsap/react';
 import { animateWithGsap } from '../utils/animations';
+import gsap from 'gsap';
 
 const HowItWorks = () => {
   const videoRef = useRef();
